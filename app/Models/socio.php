4c025da6fe67abf545b8928c548eaa9e30d13feb
CaroAuadt <62910092+CaroAuadt@users.noreply.github.com>
2022-09-08 15:41:36 -0300

    Update socio.php

--- app/Models/socio.php
+++ app/Models/socio.php
@@ -2,21 +2,54 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class socio extends Model
+/**
+ * Class Socio
+ *
+ * @property $id
+ * @property $dni
+ * @property $nombre
+ * @property $apellido
+ * @property $celular
+ * @property $email
+ * @property $deleted_at
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Reserva[] $reservas
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Socio extends Model
 {
-    use HasFactory;
     use SoftDeletes;
+
+    static $rules = [
+		'dni' => 'required',
+		'nombre' => 'required',
+		'apellido' => 'required',
+		'celular' => 'required',
+    ];
 
     protected $perPage = 20;
 
-    protected $table='socios';
-    protected $fillable=['dni','nombre','apellido','celular','email'];
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['dni','nombre','apellido','celular','email'];
 
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function reservas()
+    {
+        return $this->hasMany('App\Models\Reserva', 'socio_id', 'id');
+    }
     
-
 
 }
